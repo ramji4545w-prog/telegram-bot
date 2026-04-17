ee9b581d6430bce5bfbfad3b9c14ea0a01d0e3fa
ramji4545w-prog <ramji4545w@gmail.com>
2026-04-17 17:25:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,7 @@ from flask import Flask
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("BOT_TOKEN")
-
+TOKEN = os.getenv("8394853752:AAHvuIrKLQe2dIAewLnxj6DgypkYHvIw2-8")
 
 # ------------------ TELEGRAM BOT ------------------
 
